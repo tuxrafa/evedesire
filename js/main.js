e395b15b4ca0d8f4c1df49cab8b1eb8be57e3323
Rafael Henrique <rafael.henrique@EFs-iMac03.local>
2018-10-10 22:18:23 -0300

    v1

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ function trocaVideo(obj) {
   url = "https://www.youtube.com/embed/"+$(obj).attr("video")+"?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1";
   $('#mainVideoPlayer').attr('src',url);
 }
+
+$(".fotoChange").click(function() {trocaFoto(this)});
+function trocaFoto(obj) {
+  url = obj.src;
+  $('#mainFoto').attr('src',url);
+}
